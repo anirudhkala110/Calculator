9150c0499a3921138d8b01771e76f6b240821b3a
Abhay Kumar <shindheabhay@gmail.com>
2023-08-13 12:59:12 +0530

    calculator

---
+++ javscript.js
@@ -0,0 +1,28 @@
+//Using 'const' instead of 'let' for variables that don't change.
+const input = document.getElementById('inputBox');
+const buttons = document.querySelectorAll('button');
+
+let string = "";
+let arr = Array.from(buttons);
+arr.forEach(button => {
+    button.addEventListener("click", (e)=>{
+           if(e.target.innerHTML == '='){
+            string=eval(string);
+            input.value = string;
+           }
+
+           else if(e.target.innerHTML == 'AC'){
+            string = "";
+            input.value = string;
+           }
+           else if(e.target.innerHTML=='DEL'){
+            string = string.slice(0, -1);
+            input.value = string;
+           }
+           else{
+            string += e.target.innerHTML;
+            input.value = string;
+           }
+
+    })
+})
